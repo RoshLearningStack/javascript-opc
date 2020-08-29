d85c38ef90f0bcf67c4c1971477fc9b3f6ea06dc
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-29 16:27:32 +1200

    Constructor Function

--- demo.js
+++ demo.js
@@ -24,18 +24,30 @@
   //   }
   // }
 
-  let healthStats = {
-    height: 68,
-    weight: 150
-  };
+  // let healthStats = {
+  //   height: 68,
+  //   weight: 150
+  // };
 
-  // merge objects in immutable way
-  function mergeHealthStats(person, healthstats) {
-    return Object.assign({}, person, healthStats);
+  // // merge objects in immutable way
+  // function mergeHealthStats(person, healthstats) {
+  //   return Object.assign({}, person, healthStats);
+  // }
+  // let mergedPerson = mergeHealthStats(person1, healthStats);
+  // display(mergedPerson);
+  // display(person1);
+
+  // Constructor functions
+
+  function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age,
+    this.isAdult = function() {return this.age > 18}
   }
-  let mergedPerson = mergeHealthStats(person1, healthStats);
-  display(mergedPerson);
-  display(person1);
-
+  let personJim = new Person('Jim', 'Cooper', 29);
+  let personSofia = new Person('Sofia', 'Cooper', 17);
+  display(personJim);
+  display(personSofia);
 
 })();
