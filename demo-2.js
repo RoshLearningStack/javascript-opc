36a77305eac4e851039a55f8c0ed09660601c281
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-29 23:06:00 +1200

    Getters and Setters

--- demo-2.js
+++ demo-2.js
@@ -1,11 +1,11 @@
 'use strict';
 (function () {
  
-    let person = {
-        firstName: 'Jim',
-        lastName: 'Cooper',
-        age : 29
-    };
+    // let person = {
+    //     firstName: 'Jim',
+    //     lastName: 'Cooper',
+    //     age : 29
+    // };
     
     // Enumerable
     // Object.defineProperty(person, 'firstName', {enumerable: false});
@@ -28,11 +28,35 @@
     // person.firstName = 'Roshan';
 
     // Configurable
-    Object.defineProperty(person, 'firstName', {configurable: false});
-    Object.defineProperty(person, 'firstName', {enumerable: true});
+    // Object.defineProperty(person, 'firstName', {configurable: false});
+    // Object.defineProperty(person, 'firstName', {enumerable: true});
 
-    delete person.firstName;
-    display(person);
+    // delete person.firstName;
+    // display(person);
 
+    // Setters and Getters
+    let person = {
+        name : {
+            firstName: 'Jim',
+            lastName: 'Cooper'
+        },        
+        age : 29
+    };
+
+    Object.defineProperty(person, 'fullName', {
+        get: function () {
+            return this.name.firstName + ' ' + this.name.lastName;
+        },
+        set: function (value) {
+            var nameParts = value.split(' ');
+            this.name.firstName = nameParts[0];
+            this.name.lastName = nameParts[1];
+        }
+    });
+
+    person.fullName = 'Roshan Deniyage';
+    display(person.fullName);
+    display(person.name.firstName);
+    display(person.name.lastName);
 
 })();
