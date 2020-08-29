a47ae2464e46151be4cd99e9f9c84e6e74db457e
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-29 23:56:34 +1200

    Multiple level of inheritance

--- demo-3.js
+++ demo-3.js
@@ -5,34 +5,16 @@
 //    let myFunction = function () {
        
 //    } 
-//    display(myFunction.prototype);
-
-//    let person = {firstName: 'Jim'};
-//    display(person.prototype);
-//    display(person.__proto__);
-
 function Person(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
 }
-Person.prototype.age = 29;
-
-// display(Person.prototype);
+Person.prototype.age = 4;
 
 let jim = new Person('Jim', 'Cooper');
-let sofia = new Person('Sofia', 'Cooper');
-// sofia.__proto__.age = 23;
-// display(jim.__proto__);
-// display(sofia.__proto__);
-// display(Person.prototype === jim.__proto__);
 
-Person.prototype = {age : 18};
-let kris = new Person('Kris', 'Cooper');
-
-display(Person.prototype);
-display(jim.age);
-display(sofia.age);
-display(kris.age);
-
+display(jim.__proto__);
+display(jim.__proto__.__proto__);
+display(jim.__proto__.__proto__.__proto__);
 
 })();
