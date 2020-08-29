a9f1e103cfcf8760b0667973c176e3f148e36595
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-29 16:20:08 +1200

    Object assign and immutablity

--- demo.js
+++ demo.js
@@ -11,17 +11,31 @@
 
   // registerUser('Jim', 'Cooper');
 
-  let person = {
+  let person1 = {
     firstName: 'Jim',
     lastName: 'Cooper',
     age: 18,
     isAdult() {return this.age > 18}
   };
   
-  for (const propertyName in person) {
-    if (person.hasOwnProperty(propertyName)) {
-      display(propertyName);      
-    }
+  // for (const propertyName in person) {
+  //   if (person.hasOwnProperty(propertyName)) {
+  //     display(propertyName);      
+  //   }
+  // }
+
+  let healthStats = {
+    height: 68,
+    weight: 150
+  };
+
+  // merge objects in immutable way
+  function mergeHealthStats(person, healthstats) {
+    return Object.assign({}, person, healthStats);
   }
+  let mergedPerson = mergeHealthStats(person1, healthStats);
+  display(mergedPerson);
+  display(person1);
+
 
 })();
