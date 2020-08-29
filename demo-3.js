34452209c1f4698130036839010cb57a5ef45706
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-30 00:13:19 +1200

    Prototype inheritance

--- demo-3.js
+++ demo-3.js
@@ -5,16 +5,38 @@
 //    let myFunction = function () {
        
 //    } 
-function Person(firstName, lastName) {
+function Person(firstName, lastName, age) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.age = age;
+    Object.defineProperty(this, 'fullName', {
+        get: function() {
+           return this.firstName + ' ' + this.lastName; 
+        },
+        enumerable: true
+    })
+};
+
+function Student(firstName, lastName, age) {
+    Person.call(this, firstName, lastName, age);
+    this._enrolledCourses = [],
+
+    this.enroll = function(courseId) {
+        this._enrolledCourses.push(courseId);
+    };
+
+    this.getCourses = function() {
+       return this.fullName + "'s enrolled courses are: " + this._enrolledCourses;
+    }
 }
-Person.prototype.age = 4;
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
 
-let jim = new Person('Jim', 'Cooper');
+let jimStudent = new Student('Jim', 'Cooper', 29);
+jimStudent.enroll('CS205');
+jimStudent.enroll('CS206');
+jimStudent.enroll('CS207');
+display(jimStudent.getCourses());
 
-display(jim.__proto__);
-display(jim.__proto__.__proto__);
-display(jim.__proto__.__proto__.__proto__);
 
 })();
