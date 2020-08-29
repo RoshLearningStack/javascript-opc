52b68c377bc1f3d072d1b754c2f5742d6dc5f92c
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-30 00:14:02 +1200

    Classes

--- utils.js
+++ utils.js
@@ -116,7 +116,7 @@ let reloadJS = () => {
 
   let newScriptTag = document.createElement('script');
   newScriptTag.id = 'script';
-  newScriptTag.src = 'demo-3.js';
+  newScriptTag.src = 'demo-4.js';
   newScriptTag.textContent = '//script';
   var body = document.getElementsByTagName('body')[0];
 
