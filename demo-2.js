1893c2980beaea67007a89b3b48bdb76cf97192e
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-29 22:46:47 +1200

    Enumerable atrribute

--- demo-2.js
+++ demo-2.js
@@ -7,19 +7,26 @@
         age : 29
     };
     
+    // Enumerable
+    Object.defineProperty(person, 'firstName', {enumerable: false});
     // properties
-    // for (const propetyName in person) {
-    //     display(propetyName + ': ' + person[propetyName]);
-    // }
+    for (const propetyName in person) {
+        display(propetyName + ': ' + person[propetyName]);
+    }
+    display(Object.keys(person));
+    display(JSON.stringify(person));
 
     // Property discriptor
     // Change property writable
-    Object.defineProperty(person, 'firstName', {writable: false}); 
+    // Object.defineProperty(person, 'firstName', {writable: false}); 
 
     // Freeze properties
     // Object.freeze(person.name);
 
-    display(Object.getOwnPropertyDescriptor(person, "firstName"));
+    // display(Object.getOwnPropertyDescriptor(person, "firstName"));
     // Try to update read only property
     // person.firstName = 'Roshan';
+
+
+
 })();
