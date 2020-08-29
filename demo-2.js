b75a9f10bda7e14bf0e79e76130a3f9b35105f7f
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-29 16:38:55 +1200

    Access properties

---
+++ demo-2.js
@@ -0,0 +1,14 @@
+'use strict';
+(function () {
+ 
+    let person = {
+        firstName: 'Jim',
+        lastName: 'Cooper',
+        age : 29
+    };
+    
+    // properties
+    for (const propetyName in person) {
+        display(propetyName + ': ' + person[propetyName]);
+    }
+})();
