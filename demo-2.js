6dee97e849506d45b60680be6ec9a2f087ee4cc5
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-29 16:44:49 +1200

    Writable attribute

--- demo-2.js
+++ demo-2.js
@@ -13,5 +13,13 @@
     // }
 
     // Property discriptor
+    // Change property writable
+    Object.defineProperty(person, 'firstName', {writable: false}); 
+
+    // Freeze properties
+    // Object.freeze(person.name);
+
     display(Object.getOwnPropertyDescriptor(person, "firstName"));
+    // Try to update read only property
+    // person.firstName = 'Roshan';
 })();
