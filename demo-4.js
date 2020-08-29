f8c0f58fbc9c91e5b8d35bedfee68f3555e21607
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-30 00:20:33 +1200

    Create object with class

---
+++ demo-4.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function () {
+    
+    class Person {
+        constructor(firstName, lastName, age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+    }
+
+    let jim = new Person('Jim', 'Cooper', 29);
+    display(jim);
+
+})();
